6b73ff0c2768accba99f2b35fd7178443f67e741
farhan1086 <farhan1086@FARHAN>
2020-05-09 13:15:27 +0530

    Add project files.

---
+++ FileResultActionsCoreMvc_Demo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+using System.Text;
+
+namespace FileResultActionsCoreMvc_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult DownloadFile()
+        {
+            return File("/Files/File Result.pdf", "text/plain", "File Result.pdf");
+        }
+
+        public FileContentResult DownloadContent()
+        {
+            var myfile = System.IO.File.ReadAllBytes("wwwroot/Files/FileContentResult.pdf");
+            return new FileContentResult(myfile, "application/pdf");
+        }
+
+        public FileStreamResult CreateFile()
+        {
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes("Hello World"));
+            return new FileStreamResult(stream, new MediaTypeHeaderValue("text/plain"))
+            {
+                FileDownloadName = "test.txt"
+            };
+        }
+        public VirtualFileResult VirtualFileResult()
+        {
+            return new VirtualFileResult("/Files/PhysicalFileResult.pdf", "application/pdf");
+        }
+
+        public PhysicalFileResult PhysicalFileResult()
+        {
+            return new PhysicalFileResult(_environment.ContentRootPath + "/wwwroot/Files/PhysicalFileResult.pdf", "application/pdf");
+        }
+
+    }
+}
